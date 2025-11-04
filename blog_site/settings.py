e8f7a68530f8d4314288f68960dbba4d916ca4c2
Mosaddik Habib <mosaddikhabib.bsfmstu@gmail.com>
2025-11-04 11:12:20 +0600

    logo changed

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ckeditor',
-    'ckeditor_uploader',
+    # 'ckeditor',
+    # 'ckeditor_uploader',
     'blog',  # Our blog app
 ]
 
